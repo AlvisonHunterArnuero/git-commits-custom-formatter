7120aaa3c7b89fdabf33e265257aa693829c51e3
AlvisonHunterArnuero <alvison@gmail.com>
2025-02-08 00:17:17 -0600

    chore(global): added separated commits data array of objects

---
+++ src/app/commitsData.ts
@@ -0,0 +1,114 @@
+export const commitEmojis = [
+    {
+        emoji: '🔥',
+        title: 'Remove code or files',
+        description: 'e.g., removing deprecated files or code',
+    },
+    {
+        emoji: '🎉',
+        title: 'Initial commit',
+        description: 'e.g., when starting a new project',
+    },
+    {
+        emoji: '✨',
+        title: 'Introduce new features',
+        description: 'e.g., adding a new functionality or feature',
+    },
+    {
+        emoji: '🐛',
+        title: 'Fix a bug',
+        description: 'e.g., resolving a bug in the code',
+    },
+    {
+        emoji: '📝',
+        title: 'Update documentation',
+        description: 'e.g., editing README, comments, or documentation',
+    },
+    {
+        emoji: '🚀',
+        title: 'Deploy features',
+        description: 'e.g., preparing for a deployment or release',
+    },
+    {
+        emoji: '💄',
+        title: 'Update UI/Cosmetic',
+        description: 'e.g., improving the UI or updating styles',
+    },
+    {
+        emoji: '🚧',
+        title: 'Work in progress',
+        description:
+            'e.g., a commit in a branch that is still under development',
+    },
+    {
+        emoji: '🔧',
+        title: 'Update configurations',
+        description:
+            'e.g., changing configuration files, build scripts',
+    },
+    {
+        emoji: '♻️',
+        title: 'Refactor code',
+        description:
+            'e.g., restructuring or refactoring code without changing functionality',
+    },
+    {
+        emoji: '✅',
+        title: 'Add tests',
+        description: 'e.g., adding or updating unit tests',
+    },
+    {
+        emoji: '🗑️',
+        title: 'Remove unnecessary files',
+        description: 'e.g., deleting files that are no longer needed',
+    },
+    {
+        emoji: '⚡',
+        title: 'Improve performance',
+        description: 'e.g., optimizing code for better performance',
+    },
+    {
+        emoji: '🐎',
+        title: 'Improve performance',
+        description:
+            'similar to ⚡ but often used for significant performance improvements',
+    },
+    {
+        emoji: '🔒',
+        title: 'Fix security issues',
+        description: 'e.g., patching security vulnerabilities',
+    },
+    {
+        emoji: '🌐',
+        title: 'Internationalization',
+        description: 'e.g., adding or improving translations',
+    },
+    {
+        emoji: '🛠',
+        title: 'Tooling',
+        description:
+            'e.g., adding or updating tooling, like linters or CI/CD configurations',
+    },
+    {
+        emoji: '💚',
+        title: 'Fix CI build',
+        description:
+            'e.g., resolving issues that caused a CI build to fail',
+    },
+    {
+        emoji: '🗃',
+        title: 'Database changes',
+        description: 'e.g., altering database schema or migrations',
+    },
+    {
+        emoji: '🛂',
+        title: 'Authentication/Authorization',
+        description:
+            'e.g., implementing or updating authentication or authorization',
+    },
+    {
+        emoji: '👽',
+        title: 'External API changes',
+        description: 'e.g., adapting to changes in an external API',
+    },
+];
