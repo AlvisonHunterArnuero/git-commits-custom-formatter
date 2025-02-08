9782ac23c7ed0e51d3f9e34ebf3113aeb619a718
AlvisonHunterArnuero <alvison@gmail.com>
2025-02-08 01:53:55 -0600

    refactor(page): incorporated new input with label component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React from 'react';
 import { commitEmojis } from './commitsData';
+import InputWithLabel from './components/InputWithLabel';
 
 export default function Home() {
   const [commit, setCommit] = React.useState('');
@@ -8,22 +9,19 @@ export default function Home() {
   const [target, setTarget] = React.useState('global');
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen font-[family-name:var(--font-geist-sans)]">
-      <h2 className="font-bold text-3xl uppercase text-gray-400">
+    <div className='flex h-screen flex-col items-center justify-center font-[family-name:var(--font-geist-sans)]'>
+      <h2 className='text-3xl font-bold uppercase text-gray-400'>
         Commits Formatter
       </h2>
-      <div className="flex flex-col p-8 gap-4 w-screen max-w-screen-lg items-start justify-center font-[family-name:var(--font-geist-sans)]">
-        <div className="w-full flex flex-col gap-y-3 text-sky-500">
-          <label
-            htmlFor="commitSelector"
-            className="text-md uppercase"
-          >
-            Target:
+      <div className='flex w-screen max-w-screen-xl flex-col items-start justify-center gap-4 p-8 font-[family-name:var(--font-geist-sans)]'>
+        <div className='flex w-full flex-col gap-y-3 text-sky-500'>
+          <label htmlFor='commitSelector' className='text-md uppercase'>
+            Commit Type:
           </label>
           <select
-            id="commitSelector"
+            id='commitSelector'
             onChange={(e) => setCommitType(e.target.value)}
-            className="h-11 text-green-400 text-xl w-full max-w-screen-lg px-4 rounded-md bg-black border border-gray-300"
+            className='h-11 w-full max-w-screen-xl rounded-md border border-gray-300 bg-black px-4 text-xl text-green-400'
           >
             {commitEmojis.map((commit, ind) => {
               return (
@@ -34,54 +32,48 @@ export default function Home() {
             })}
           </select>
         </div>
+        <InputWithLabel
+          id='target'
+          labelText='Target:'
+          labelHtmlForId='target'
+          labelClassName='text-md uppercase'
+          inputOnChange={(e) => setTarget(e.target.value)}
+          defaultValue=''
+          placeholder='e.g., "component" or "global"'
+          inputType='text'
+          wrapperClassName='flex w-full flex-col gap-y-3 text-sky-500'
+          inputClassName='h-11 w-full max-w-screen-xl rounded-md border border-gray-300 bg-black px-4 text-xl text-green-400'
+        />
 
-        <div className="w-full flex flex-col gap-y-3 text-sky-500">
-          <label htmlFor="target" className="text-md uppercase">
-            Target:
-          </label>
-          <input
-            id="target"
-            className="bg-black border text-green-400 text-xl border-gray-300 h-11 w-full max-w-screen-lg px-4 rounded-md"
-            type="text"
-            defaultValue="component"
-            placeholder='e.g., "component" or "global"'
-            onChange={(e) => setTarget(e.target.value)}
-          />
-        </div>
-        <div className="w-full flex flex-col gap-y-3 text-sky-500">
+        <InputWithLabel
+          id='commitDescription'
+          labelText='Commit Description:'
+          labelHtmlForId='commitDescription'
+          labelClassName='text-md uppercase'
+          inputOnChange={(e) => setCommit(e.target.value)}
+          defaultValue=''
+          placeholder='e.g., "fixing a bug"'
+          inputType='text'
+          wrapperClassName='flex w-full flex-col gap-y-3 text-sky-500'
+          inputClassName='blur:outline-none blur:ring-0 h-11 w-full max-w-screen-xl rounded-md border border-gray-300 bg-black px-4 text-xl text-green-400 focus:outline-none'
+        />
+        <div className='mt-6 flex w-full flex-col gap-y-3 pb-10'>
           <label
-            htmlFor="commitDescription"
-            className="text-md uppercase"
-          >
-            Commit Description:
-          </label>
-          <input
-            id="commitDescription"
-            className="bg-black border text-green-400 text-xl border-gray-300 h-11 w-full max-w-screen-lg px-4 rounded-md focus:outline-none blur:outline-none blur:ring-0"
-            type="text"
-            defaultValue=""
-            placeholder='e.g., "fixing a bug"'
-            onChange={(e) => setCommit(e.target.value)}
-          />
-        </div>
-
-        <div className="w-full flex flex-col gap-y-3 text-sky-500 bg-gray-800 rounded-md p-6 mt-6 pb-10">
-          <label
-            htmlFor="formattedCommit"
-            className="text-yellow-200 text-md uppercase"
+            htmlFor='formattedCommit'
+            className='text-md uppercase text-yellow-200'
           >
             Formatted Commit:
           </label>
           <input
-            id="formattedCommit"
-            className="bg-black text-center text-gray-400 text-xl lowercase border-gray-600 border h-11 w-full max-w-screen-lg px-4 rounded-md"
-            type="text"
+            id='formattedCommit'
+            className='h-11 w-full max-w-screen-xl rounded-md border border-gray-600 bg-black pl-1 text-xl lowercase text-gray-400'
+            type='text'
             value={`${commitEmojis[+commitType].emoji} ${
               commitEmojis[+commitType].title
             } - (${target}): ${commit}`}
             onChange={() => null}
           />
-          <div className="text-white font-extralight italic">
+          <div className='font-extralight italic text-white'>
             once ready, you can copy the above text
           </div>
         </div>
